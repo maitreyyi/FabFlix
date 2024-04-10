ec5811b525fe80caf13344772c9f6ceca13bde24
Sharon Leo <ssleo1@uci.edu>
2024-04-10 10:04:47 -0700

    Updated page format

--- WebContent/single-movie.js
+++ WebContent/single-movie.js
@@ -47,32 +47,30 @@ function handleResult(resultData) {
     let genreTableBodyElement = jQuery("#genre_table_body");
 
     // Concatenate the html tags with resultData jsonObject to create table rows
-    for (let i = 0; i < Math.max(resultData["genres"].length, resultData["stars"].length); i++) {
-        let rowHTML = "";
-        rowHTML += "<tr>";
-        try {
-            rowHTML += "<th>" + resultData["genres"][i]["genre_name"] + "</th>";
-        }
-        catch(TypeError){
-            rowHTML += "<th></th>";
-        }
-
-        try {
-            rowHTML += "<th>" +
-            // Add a link to single-star.html with id passed with GET url parameter
-            '<a href="single-star.html?id=' + resultData["stars"][i]['star_id'] + '">'
-            + resultData["stars"][i]["star_name"] +     // display star_name for the link text
-            '</a>' +
-            "</th>";
-        }
-        catch(TypeError){
-            rowHTML += "<th></th>";
-        }
-        rowHTML += "</tr>";
-
-        // Append the row created to the table body, which will refresh the page
-        genreTableBodyElement.append(rowHTML);
+    let rowHTML = "";
+    rowHTML += "<tr><th>";
+    for (let i = 0; i < resultData["genres"].length; i++) {
+        rowHTML += resultData["genres"][i]["genre_name"]
+        if (i + 1 < resultData["genres"].length)
+            rowHTML += ", ";
     }
+    rowHTML += "</th><th>";
+
+    for (let i = 0; i < resultData["stars"].length; i++) {
+        // Add a link to single-star.html with id passed with GET url parameter
+        rowHTML +=
+        '<a href="single-star.html?id=' + resultData["stars"][i]['star_id'] + '">'
+        + resultData["stars"][i]["star_name"] +     // display star_name for the link text
+        '</a>'
+        if (i + 1 < resultData["stars"].length)
+            rowHTML += ", ";
+    }
+
+    rowHTML += "</th></tr>";
+
+    // Append the row created to the table body, which will refresh the page
+    genreTableBodyElement.append(rowHTML);
+
 }
 
 /**
