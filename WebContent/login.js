5b2f9a8fa6d04d46a8a7b6dc9567fafd63933098
Sharon Leo <ssleo1@uci.edu>
2024-04-17 20:47:10 -0700

    Updated login page to project requirements; Added access to SQL to authorize user

--- WebContent/login.js
+++ WebContent/login.js
@@ -2,24 +2,21 @@ let login_form = $("#login_form");
 
 /**
  * Handle the data returned by LoginServlet
- * @param resultDataString jsonObject
+ * @param resultData jsonObject
  */
-function handleLoginResult(resultDataString) {
-    let resultDataJson = JSON.parse(resultDataString);
-
+function handleLoginResult(resultData) {
     console.log("handle login response");
-    console.log(resultDataJson);
-    console.log(resultDataJson["status"]);
+    console.log(resultData["status"]);
 
     // If login succeeds, it will redirect the user to index.html
-    if (resultDataJson["status"] === "success") {
-        window.location.replace("index.html");
+    if (resultData["status"] === "success") {
+        window.location.replace("movie-list.html"); //Change to main page later
     } else {
         // If login fails, the web page will display
         // error messages on <div> with id "login_error_message"
         console.log("show error message");
-        console.log(resultDataJson["message"]);
-        $("#login_error_message").text(resultDataJson["message"]);
+        console.log(resultData["message"]);
+        $("#login_error_message").text(resultData["message"]);
     }
 }
 
--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -1,44 +1,106 @@
 import com.google.gson.JsonObject;
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
 public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 2L;
+
+    // Create a dataSource which registered in web.xml
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
+        response.setContentType("application/json"); // Response mime type
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
 
-        /* This example only allows username/password to be test/test
-        /  in the real project, you should talk to the database to verify username/password
-        */
-        JsonObject responseJsonObject = new JsonObject();
-        if (username.equals("anteater") && password.equals("123456")) {
-            // Login success:
+        try (Connection conn = dataSource.getConnection()) {
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
 
-            // set this user into the session
-            request.getSession().setAttribute("user", new User(username));
+            /* This example only allows username/password to be test/test
+            /  in the real project, you should talk to the database to verify username/password
+            */
+            String query = "SELECT id, password FROM customers " +
+                           "where email = ?";
 
-            responseJsonObject.addProperty("status", "success");
-            responseJsonObject.addProperty("message", "success");
+            // Declare statement for stars_query
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setString(1, username);
+            // Perform the query
+            ResultSet rs = statement.executeQuery();
+            JsonObject responseJsonObject = new JsonObject();
 
-        } else {
-            // Login fail
-            responseJsonObject.addProperty("status", "fail");
-            // Log to localhost log
-            request.getServletContext().log("Login failed");
-            // sample error messages. in practice, it is not a good idea to tell user which one is incorrect/not exist.
-            if (!username.equals("anteater")) {
-                responseJsonObject.addProperty("message", "user " + username + " doesn't exist");
-            } else {
-                responseJsonObject.addProperty("message", "incorrect password");
+            if(rs.next())
+            {
+                String return_password = rs.getString("password");
+
+                if (!password.equals(return_password)) // Double-check string comparisons
+                {
+                    // Password is wrong, login fail
+                    responseJsonObject.addProperty("status", "fail");
+                    // Log to localhost log
+                    request.getServletContext().log("Login failed");
+                    // sample error messages. in practice, it is not a good idea to tell user which one is incorrect/not exist.
+                    responseJsonObject.addProperty("message", "incorrect password");
+                }
+                else
+                {
+                    // Login success:
+                    // set this user into the session
+                    request.getSession().setAttribute("user", new User(username));
+
+                    responseJsonObject.addProperty("status", "success");
+                    responseJsonObject.addProperty("message", "success");
+
+                }
             }
+            else {
+                // Email is wrong, login fail
+                responseJsonObject.addProperty("status", "fail");
+                // Log to localhost log
+                request.getServletContext().log("Login failed");
+                // sample error messages. in practice, it is not a good idea to tell user which one is incorrect/not exist.
+                responseJsonObject.addProperty("message", "user " + username + " doesn't exist");
+            }
+            statement.close();
+            rs.close();
+            out.write(responseJsonObject.toString());
+
+        } catch (Exception e) {
+            // Write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // Log error to localhost log
+            request.getServletContext().log("Error:", e);
+            // Set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        } finally {
+            out.close();
         }
-        response.getWriter().write(responseJsonObject.toString());
     }
 }
