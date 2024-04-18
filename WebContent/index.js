0012dacaba13191434155bf52c099fb7098985e9
Sharon Leo <ssleo1@uci.edu>
2024-04-18 14:46:34 -0700

    Added code for browsing

---
+++ WebContent/index.js
@@ -0,0 +1,60 @@
+/**
+ * Handles the data returned by the API, read the jsonObject and populate data into html elements
+ * @param resultData jsonObject
+ */
+
+function handleResult(resultData) {
+
+    console.log("handleResult: populating genre hyperlinks");
+
+    // populate the alphabet browsing
+    let genreLink = jQuery("#genres");
+    let content = "";
+    for (let i = 0; i < resultData.length; i++)
+    {
+        content += '<a href="movie-list.html?genre=' + resultData[i]["genre_id"]
+                    + '">' + resultData[i]["genre_name"] + '</a>';
+        if (i + 1 < resultData.length)
+        {
+            content += " "
+        }
+    }
+    genreLink.append("<h5>"+ content + "</h5>");
+
+    console.log("handleResult: populating alphanumeric hyperlinks");
+
+    // populate the alphabet browsing
+    let alphanumericLink = jQuery("#alphanumeric");
+    content = "";
+    for (let i = 65; i < 91; i++)
+    {
+        content += '<a href="movie-list.html?start=' + String.fromCharCode(i)
+                    + '">' + String.fromCharCode(i) + '</a>';
+        if (i + 1 < 91)
+        {
+            content += " "
+        }
+    }
+    alphanumericLink.append("<h5>"+ content + "</h5>");
+
+    content = "";
+    for (let i = 0; i < 10; i++)
+    {
+        content += '<a href="movie-list.html?start=' + i + '">' + i + '</a>' + " ";
+    }
+    content += '<a href="movie-list.html?start=*">*</a>';
+    alphanumericLink.append("<h5>"+ content + "</h5>");
+
+}
+
+/**
+ * Once this .js is loaded, following scripts will be executed by the browser\
+ */
+
+// Makes the HTTP GET request and registers on success callback function handleResult
+jQuery.ajax({
+    dataType: "json",  // Setting return data type
+    method: "GET",// Setting request method
+    url: "api/index", // Setting request url, which is mapped by StarsServlet in Stars.java
+    success: (resultData) => handleResult(resultData) // Setting callback function to handle data returned successfully by the SingleStarServlet
+});
---
+++ src/GenreBrowseServlet.java
@@ -0,0 +1,96 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+// Declaring a WebServlet called SingleStarServlet, which maps to url "/api/movie-list"
+@WebServlet(name = "GenreBrowseServlet", urlPatterns = "/api/index")
+public class GenreBrowseServlet extends HttpServlet {
+    private static final long serialVersionUID = 2L;
+
+    // Create a dataSource which registered in web.xml
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("application/json"); // Response mime type
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
+        // Get a connection from dataSource and let resource manager close the connection after usage.
+        try (Connection conn = dataSource.getConnection()) {
+            // Get a connection from dataSource
+
+            // Get stars and movie information (hyperlinks)
+            // Construct a queries with parameter represented by "?"
+            String genre_query = "SELECT * from genres";
+
+            // Declare statement for stars_query
+            PreparedStatement statement = conn.prepareStatement(genre_query);
+            // Perform the query
+            ResultSet rs = statement.executeQuery();
+            // Declare object for movie information
+            JsonArray genreArray = new JsonArray();
+
+            //set up movies objects
+            while (rs.next()) {
+                JsonObject genreObj = new JsonObject();
+
+                // Convert row data into strings
+                String genreId = rs.getString("id");
+                String genreName = rs.getString("name");
+
+                // Add data as properties of the object
+                genreObj.addProperty("genre_id", genreId);
+                genreObj.addProperty("genre_name", genreName);
+
+                genreArray.add(genreObj);
+            }
+
+            // Close queries
+            rs.close();
+            statement.close();
+
+            // Write JSON string to output
+            out.write(genreArray.toString());
+            // Set response status to 200 (OK)
+            response.setStatus(200);
+
+        } catch (Exception e) {
+            // Write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // Log error to localhost log
+            request.getServletContext().log("Error:", e);
+            // Set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
+    }
+}
