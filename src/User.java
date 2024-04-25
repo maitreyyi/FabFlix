eb14b515c93603cfd67e18e853b9d71a84c83d19
Sharon Leo <ssleo1@uci.edu>
2024-04-24 20:17:01 -0700

    Added cart map

--- src/User.java
+++ src/User.java
@@ -1,3 +1,6 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * This User class only has the username field in this example.
  * You can add more attributes such as the user's shopping cart items.
@@ -5,6 +8,7 @@
 public class User {
 
     private final String username;
+    Map<String, Integer> cart = new TreeMap<>();
 
     public User(String username) {
         this.username = username;
