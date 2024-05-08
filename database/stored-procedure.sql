bfc653c167a0f849d3e2ce0b52f32803f4cdcadb
maitreyyi <matreyisinha@gmail.com>
2024-05-08 12:06:23 -0700

    Created stored-procedure.sql for add_movie

--- database/stored-procedure.sql
+++ database/stored-procedure.sql
@@ -1,11 +1,31 @@
-CREATE PROCEDURE add_movie @id varchar(10), @title varchar(100), @year int, @director varchar(100), @star_name varchar(100), @birthYear int, @genre varchar(100)
-AS
+DELIMITER //
+CREATE PROCEDURE add_movie (IN @movie_id varchar(10), IN @title varchar(100), IN @year int, IN @director varchar(100), IN @star_name varchar(100), IN @genre varchar(100) )
+
 BEGIN
-INSERT INTO movies (id, title, year, director)
-VALUES (@id, @title, @year, @director)
+	DECLARE star_id;
+	DECLARE genre_id;
+	DECLARE movie_title;
 
-    INSERT INTO stars_in_movies(starId, movieId)
-VALUES(@star_id, @movieId)
+    -- check if movie exists or not (if it doesn't exist, add into movies table)
+SELECT title INTO movie_title FROM movies WHERE id = movie_id;
+IF movie_title is NULL THEN
+		INSERT INTO movies (id, title, year, director) VALUES (@movie_id, @title, @year, @director)
+END IF;
 
-INSERT INTO stars(id, name, birthYear)
-VALUES(@star_id, @star_name, @birthYear)
+    -- If the star doesn't exist, insert into the stars table
+SELECT id INTO star_id FROM stars WHERE name = star_name;
+IF star_id IS NULL THEN
+        INSERT INTO stars (name, birthYear) VALUES (star_name, NULL);
+        SET star_id = LAST_INSERT_ID();
+INSERT INTO stars_in_movies(starId, movieId) VALUES(@star_id, @movie_d)
+END IF;
+
+	-- check if genre exists
+SELECT id INTO genre_id FROM genres WHERE name = genre
+    IF genre_id is NULL THEN
+INSERT INTO genres_in_movies(genreId, movieId)
+VALUES(@genre_id, @movie_id)
+END IF;
+
+END
+DELIMITER //;
